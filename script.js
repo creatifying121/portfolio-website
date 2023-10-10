5fc3cc7b26c3db9eecc2c44f93efcefbd4fb99e1
creatifying121 <shivani.raichandani121@gmail.com>
2023-10-10 20:21:25 +0530

    hero section and about section

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var typeData = new Typed(".role", {
+    strings: [
+      "Frontend Developer",
+      "Full Stack Developer",
+      "UI-UX Designer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
