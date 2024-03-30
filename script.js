c28d7b7f894ca1c2c32a4525e1e602d773cb4980
creatifying121 <shivani.raichandani121@gmail.com>
2024-03-30 22:48:32 +0530

    commit portfolio

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links"); 
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 var typeData = new Typed(".role", {
-    strings: [
-      "Frontend Developer",
-      "Full Stack Developer",
-      "UI-UX Designer",
-      "Coder",
-    ],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1000,
-  });
+  strings: [
+    "Software Engineer",
+    "Frontend Developer",
+    "Web Designer",
+    "Full-time Learner",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
