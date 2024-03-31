d197badc964ba830ec2ecc22b20993f051359a10
creatifying121 <shivani.raichandani121@gmail.com>
2024-04-01 00:09:42 +0530

    commit contact form

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+// dynamic text
 var typeData = new Typed(".role", {
   strings: [
     "Software Engineer",
@@ -50,3 +51,27 @@ const observer = new IntersectionObserver(
   }
 );
 observer.observe(sectionHeroEl);
+
+// contact form configuration
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "shivaniraichandani5@gmail.com",
+    Password: "17F37D9533A044C91E6426CD639D3B9D8AA3",
+    To: "shivaniraichandani5@gmail.com",
+    From: "shivaniraichandani5@gmail.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  sendEmail();
+});
